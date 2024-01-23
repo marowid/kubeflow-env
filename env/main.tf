0635a4f29daa6b3a81c944b95ed6e68975e82609
marowid <marowid@gmail.com>
2024-01-23 07:08:31 +0100

    .

--- env/main.tf
+++ env/main.tf
@@ -32,7 +32,7 @@ variable "prefix" {
   default = "info-"
 }
 
-resource "google_compute_instance" "bpk-llm" {
+resource "google_compute_instance" "llm" {
   boot_disk {
     auto_delete = true
     device_name = "${var.prefix}llm"
@@ -53,7 +53,7 @@ resource "google_compute_instance" "bpk-llm" {
   machine_type = "e2-standard-32"
 
   metadata = {
-    ssh-keys = "ubuntu:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDDrfL0VNEve9Ve/pH1JQcC1Nj2WshN1UJ86r+b0SPjoq1u20eDy8K0ESBfzTXibs3FavKYUI9cKUsGwvsECd+xwFZPW4vG/AN2wU5e9Dq+789pMxJeQIbe1AocWUuMhXU8RB0no97Xp2/w0dYuXXu5aLNDuGa0HlpRhINCmWRInIgjjG6xuatcEaI6GufjnwzxUMYVbptvtFwK0RpPKGKOQFXksso2jL+Pv4ozFUm+2dpeYIjxzN0785lM5loKHJsU+/FCj6cDoqINWnotK3oBQ5E20kgcBVgOu5MY/wx8P2Yv2Afln6rjRZaj/o9Xt3qMKmbMP0ExQaHLcEMfJlqzOyxwzKPTgEj3peWwLJPO2K6RS+htEMwjvbUvROTyWYTjwXy44eqdfYmYJDI3HP2czhz53XotxS5Zw2+PZlmNiwSttdl0EE0Eu4qpeOz5W+I6vQUFocnGhhZ8vSdbvMg3IgMbmGfpnfFnlwwDQHK295Qnwyuq1gd9XqrIha9BvgdU1lmVS1hHt5PUtAXQe45FOwhL8YqZG+Q2zIyMhycKxmDzUkST7/+loppTO0ZwjRz1gzKVbPVFOvs4fssa51oWVaeGqk8P07MPlkDXOlcQ8egOUie9p70g4FUmRXzCWX5SML+cQn3A9lDvChy/auRUhU2INgZKzKXGSnqT+qxZWQ== ubuntu\nubuntu:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDvq4/W+nNh98rZsm5yQtDxyHFdcfEITg1Go1xMtma51dF4Q0bwVXmWr9XXujU+qWIdI6kGIeEDVQuScn0TA6fB4M21BfP/91vmapYnpuGipkjHcbfHwvj1u5FRGOv3o3suzzqYDmvxw3subIRFYycXEfGnsRSU/+8dDv0C2BHaEEdrw+qbJqQZJHBdM1o6Pxk+g6UiH9EC2rJjfyXm+8XTse794moSe7uXTI6PW8SQIbMR4r7EPeznxfj8rqzXQxT34igI4KIoBLfoZY+uQOQmhzB7QVksyfbJ5av3DJjWPhf1uFoTu7iGj1xeXiEauxnrG4wsuopqMUGpT6G1FqbHJr2oVHMsRbSZFtdpso25UUwqXBraxUUTo4JjNpoxG3+M3d4M7feQgAd0hX7m8+yRZrwebEAaxqCTdTE8yi8Nu6fFEQP/NAevE7NtaDbvjagJFn//R28ICy9VZgF2O+VxezZ3jdiyRhty9QW6rA29FX/sYD1NVZ8gPoNNYrKALI0= ubuntu"
+    ssh-keys = "key"
   }
 
   name = "${var.prefix}llm"
